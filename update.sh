b82873d5568c22d39be034fbaf90b98492588d04
Ashwath Sankar <asankar21@gmail.com>
2019-06-18 14:39:36 -0400

    added description to update.sh

--- update.sh
+++ update.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+#This scipt update the Singularity notebooks to the latest version available on CellOrganizer.
+#Singularity notebooks are workflows that perform multiple functions in the same script. 
+
 url='http://www.cellorganizer.org/Downloads/v2.8.0/docker/notebooks.txt'
-# DIRECTORY='notebooks'
-# if [ ! -d "$DIRECTORY" ]; then
-# 	mkdir notebooks && cd notebooks
-# else
-# 	echo 'here'
-# 	cd notebooks
-# fi
+DIRECTORY='notebooks'
+if [ ! -d "$DIRECTORY" ]; then
+	mkdir notebooks && cd notebooks
+else
+ 	echo 'here'
+ 	cd notebooks
+fi
 
 wget -nc --quiet -O file.txt $url
 while read -r line; do
